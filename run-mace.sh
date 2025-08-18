0619c6727132c3fa61b1464a39a1af19bea3652f
Miroslav Lebeda <149959159+bracerino@users.noreply.github.com>
2025-08-18 18:05:46 +0200

    Update run-mace.sh

--- run-mace.sh
+++ run-mace.sh
@@ -5,4 +5,4 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 # Activate virtual environment
 source "$SCRIPT_DIR/mace_env/bin/activate"
 
-streamlit $SCRIPT_DIR/run app.py
+streamlit run $SCRIPT_DIR/app.py
