ce4980c27f769212c1a880e2dd74e8afdb208759
Miroslav Lebeda <149959159+bracerino@users.noreply.github.com>
2026-06-06 21:14:21 +0200

    Update version number to v0.9.5.2

--- app.py
+++ app.py
@@ -4444,7 +4444,7 @@ with colx1:
             padding: 4px 11px;
             border-radius: 10px;
         ">
-            v0.9.5.1 · 5/31/2026
+            v0.9.5.2 · 6/6/2026
         </span>
     </div>
     """, unsafe_allow_html=True)
